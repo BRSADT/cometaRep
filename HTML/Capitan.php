8c54c2fc64f4fd064643ad09f39ee5f82c5d6713
BRSADT <brendasamant@hotmail.com>
2018-11-01 16:35:23 -0600

    un fallo
    creo que se corrigio. para que cualquier foto se pueda poner

--- HTML/Capitan.php
+++ HTML/Capitan.php
@@ -32,7 +32,7 @@
 		<div class="frente">
 
       <div id="logo">
-      <img  src="../imagenes/Fotografias/brendaAvila.png" alt="Italian Trulli">
+      <img  src="../imagenes/Fotografias/capitanaAvila.png" alt="Italian Trulli">
 
 
       </div>
